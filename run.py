3c4731c6d70c282fa22ad23bd42cc1399d58e2d0
Ekta Mehra <mehra.ekta95@gmail.com>
2024-10-11 12:37:19 +0000

    Set up the Tic-Tac-Toe structure

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def display_board(board):
+
+def player_move(board):
+
+def bot_move(board):
+
+def main():
