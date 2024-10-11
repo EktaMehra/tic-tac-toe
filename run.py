93fb44e129c2632bf538bbe5d632595ff1c864f5
Ekta Mehra <mehra.ekta95@gmail.com>
2024-10-11 13:48:16 +0000

    Implement player input handling and validation

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import random
 
 def display_board(board):
     """ Display current game board"""
-    # create board to implement function to initialize and display the game board
     for row in board:
         print(" | ".join(row))
         print("-" * 5)
@@ -10,7 +9,18 @@ def display_board(board):
 def player_move(board):
     """ Get the player's move and update the board"""
     # player input handling and validation
-    
+    while true: 
+        try: 
+            move = int(input("Enter you move(1-9):"))  -1
+            row, col = divmod(move, 3)
+            if board[row][col] == " ":
+                board[row][col] == "X"
+                break
+            else:
+                print("Invalid move! Space is already taken.")
+        except (ValueError, IndexError):
+            print("Invalid input! Please enter a number between 1 and 9.")
+
 
 
 def bot_move(board):
@@ -20,3 +30,4 @@ def bot_move(board):
 
 def main():
     """ Main function to run Tic-Tac-Toe game"""
+    
