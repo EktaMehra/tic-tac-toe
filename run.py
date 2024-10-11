bd8995564cb946426ec7310e848569a04157c362
Ekta Mehra <mehra.ekta95@gmail.com>
2024-10-11 12:58:45 +0000

    Create board to implement function to initialize and display the game board

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 import random
 
 def display_board(board):
+    """ Display current game board"""
+    # create board to implement function to initialize and display the game board
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
 
 def player_move(board):
+    """ Get the player's move and update the board"""
+    # player input handling and validation
+    
+
 
 def bot_move(board):
+    """ Make random move for the bot"""
+    # implimant bot's random move function
+
 
 def main():
+    """ Main function to run Tic-Tac-Toe game"""
